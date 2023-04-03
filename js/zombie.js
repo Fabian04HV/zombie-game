193bc200d503eaec4a969570a2e450485ac3be89
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-03 14:56:33 +0200

    zombies walk towards player

--- js/zombie.js
+++ js/zombie.js
@@ -10,17 +10,23 @@ class Zombie{
     this.followPlayer()
   }
   facePlayer(){
-    let dx = game.player.x - this.x - this.width / 2;
-    let dy = game.player.y - this.y - this.height / 2; 
-    let angle = atan2(dy, dx);
-    push();
-    translate(this.x + this.width / 2, this.y + this.height / 2);
-    rotate(angle);
-    image(game.zombieImage, -this.width / 2, -this.height / 2, this.width, this.height);
-    pop();
+    let dx = game.player.x - this.x 
+    let dy = game.player.y - this.y
+    let angle = atan2(dy, dx)
+    push()
+    translate(this.x + this.width / 2, this.y + this.height / 2)
+    rotate(angle)
+    image(game.zombieImage, -this.width / 2, -this.height / 2, this.width, this.height)
+    pop()
   }
-  followPlayer(){
-    let playerPosition = [game.player.x, game.player.y]
-    
-  }
+  followPlayer() {
+    const speed = 2
+    const dx = game.player.x + game.player.width/2 - this.x - this.width / 2
+    const dy = game.player.y + game.player.height/2 - this.y - this.height / 2
+    const angle = atan2(dy, dx)
+    const vx = cos(angle) * speed
+    const vy = sin(angle) * speed
+    this.x += vx
+    this.y += vy
+  } 
 }
