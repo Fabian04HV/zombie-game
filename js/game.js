21fda17be68cc28781ee06b0e0e8b2fb27c39252
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-03 14:20:15 +0200

    zombies spawn random at the edges

--- js/game.js
+++ js/game.js
@@ -21,9 +21,30 @@ class Game{
   }
   spawnZombies(){
     setInterval(function(){
-      let randX = Math.floor(Math.random() * (width - 0) + 0)
+      let randX = Math.floor(Math.random() * width)
+      let randY = Math.floor(Math.random() * height)
 
-      const zombie = new Zombie(randX, 0)
+      let randSide = Math.floor(Math.random() * 4)
+      let zombie
+
+      switch(randSide){
+        case 0:
+          console.log("top")
+          zombie = new Zombie(randX, 0) 
+          break;
+        case 1: 
+          console.log("bottom")
+          zombie = new Zombie(randX, height - 100) 
+          break;
+        case 2: 
+          console.log("left")
+          zombie = new Zombie(0, randY) 
+          break;
+        case 3: 
+          console.log("right")
+          zombie = new Zombie(width - 100, randY) 
+          break;
+      }
       game.zombies.push(zombie)
       console.log("zombie spawned")
     }, 1000)
