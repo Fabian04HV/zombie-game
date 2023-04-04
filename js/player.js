b36af276f7589f1850f14ee420ff3e8c5aa3dc7c
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-04 11:26:46 +0200

    implement red tint on player image when being bit

--- js/player.js
+++ js/player.js
@@ -7,8 +7,11 @@ class Player{
     this.height = 100
     this.bullets = []
     this.health = 3
+    this.redTint = false
+    this.momentOfDamage 
   }
   aim() {
+    // console.log(this.redTint)
     //rotate the player towards cursor position
     let dx = mouseX - this.x - this.width / 2;
     let dy = mouseY - this.y - this.height / 2; 
@@ -16,7 +19,16 @@ class Player{
     push();
     translate(this.x + this.width / 2, this.y + this.height / 2);
     rotate(angle);
+    if(this.redTint){
+      tint(255, 0 , 0)
+    }
     image(game.playerImage, -this.width / 2, -this.height / 2, this.width, this.height);
+    if(!this.redTint){
+      noTint()
+    }
+    if(frameCount >= this.momentOfDamage + 20){
+      this.redTint = false
+    }
     pop();
   }
   shoot(){
@@ -28,7 +40,7 @@ class Player{
     const playerX = this.x + (this.width / 2)
     const playerY = this.y + (this.height / 2)
 
-    game.zombies.forEach(function(zombie){
+    game.zombies.forEach((zombie) =>{
       let zombieX = zombie.x + (zombie.width / 2)
       let zombieY = zombie.y + (zombie.height / 2)
       let distance = dist(zombieX, zombieY, playerX, playerY)
@@ -36,6 +48,11 @@ class Player{
         game.zombies.splice(game.zombies.indexOf(zombie), 1)
         console.log(zombie + "BITE")
         game.player.health--
+        
+        console.log(this.redTint)
+        this.redTint = true 
+        this.momentOfDamage = frameCount
+
         let currentHealth = healthText.innerText
         let healthMinusOne = currentHealth.slice(0, -1)
         healthText.innerText = healthMinusOne
