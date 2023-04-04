093f331bcd49df574b2a508a400339b1905f91f1
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-04 14:41:14 +0200

    add zombie walk animation

--- js/game.js
+++ js/game.js
@@ -13,10 +13,53 @@ class Game{
     this.spawnInterval = 60; // spawn a zombie every 60 frames
     this.spawnIntervalDecrement = 0.035;
     this.lastSpawnFrame = 0;
+
+
+    this.zombieImagesIndex = 0
+    this.zombieImages = [
+      this.zombieImage0,
+      this.zombieImage1,
+      this.zombieImage2,
+      this.zombieImage3,
+      this.zombieImage4,
+      this.zombieImage5,
+      this.zombieImage6,
+      this.zombieImage7,
+      this.zombieImage8,
+      this.zombieImage9,
+      this.zombieImage10,
+      this.zombieImage11,
+      this.zombieImage12,
+      this.zombieImage13,
+      this.zombieImage14,
+      this.zombieImage15,
+      this.zombieImage16
+    ]
+    
+
   }
   preload(){
     this.playerImage = loadImage("../Top_Down_Survivor/handgun/idle/survivor-idle_handgun_0.png")
-    this.zombieImage = loadImage("../images/export/skeleton-attack_0.png")
+    
+    this.zombieImage = loadImage("../images/zombie-walking.gif")
+
+    this.zombieImages[0] = loadImage("../images/export/skeleton-move_0.png")
+    this.zombieImages[1] = loadImage("../images/export/skeleton-move_1.png")
+    this.zombieImages[2] = loadImage("../images/export/skeleton-move_2.png")
+    this.zombieImages[3] = loadImage("../images/export/skeleton-move_3.png")
+    this.zombieImages[4] = loadImage("../images/export/skeleton-move_4.png")
+    this.zombieImages[5] = loadImage("../images/export/skeleton-move_5.png")
+    this.zombieImages[6] = loadImage("../images/export/skeleton-move_6.png")
+    this.zombieImages[7] = loadImage("../images/export/skeleton-move_7.png")
+    this.zombieImages[8] = loadImage("../images/export/skeleton-move_8.png")
+    this.zombieImages[9] = loadImage("../images/export/skeleton-move_9.png")
+    this.zombieImages[10] = loadImage("../images/export/skeleton-move_10.png")
+    this.zombieImages[11] = loadImage("../images/export/skeleton-move_11.png")
+    this.zombieImages[12] = loadImage("../images/export/skeleton-move_12.png")
+    this.zombieImages[13] = loadImage("../images/export/skeleton-move_13.png")
+    this.zombieImages[14] = loadImage("../images/export/skeleton-move_14.png")
+    this.zombieImages[15] = loadImage("../images/export/skeleton-move_15.png")
+    this.zombieImages[16] = loadImage("../images/export/skeleton-move_16.png")
   }
   draw(){
     if(gameOver) {
@@ -25,6 +68,11 @@ class Game{
     }
     else{
       background("#101111")
+
+      if(frameCount % 4 == 0){
+        this.zombieImagesIndex++
+      }
+      this.zombieWalkAnim()
       this.player.draw()
       this.spawnZombies()
       this.zombies.forEach(zombie =>{
@@ -65,5 +113,11 @@ class Game{
     scoreTexts.forEach(text =>{
       text.innerText = this.score
     })
+  }
+  zombieWalkAnim(){
+    if(this.zombieImagesIndex == 17){
+      this.zombieImagesIndex = 0
+    }
+    this.zombieImage = this.zombieImages[this.zombieImagesIndex] 
   }
 }
